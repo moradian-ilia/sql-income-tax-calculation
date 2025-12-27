6cc6b4df027b8756cb832458cb9accd799145107
Abolfazl Moradian <moradian@gmail.com>
2025-12-27 19:50:20 +0200

    Update test_queries.sql

--- test_queries.sql
+++ test_queries.sql
@@ -1,4 +1,5 @@
-SELECT dbo.CalculateIncomeTax(8000)  AS Tax_8000;
-SELECT dbo.CalculateIncomeTax(25000) AS Tax_25000;
-SELECT dbo.CalculateIncomeTax(75000) AS Tax_75000;
-
+SELECT dbo.CalculateIncomeTax(0)       AS Tax_0;
+SELECT dbo.CalculateIncomeTax(8000)    AS Tax_8000;
+SELECT dbo.CalculateIncomeTax(25000)   AS Tax_25000;
+SELECT dbo.CalculateIncomeTax(75000)   AS Tax_75000;
+SELECT dbo.CalculateIncomeTax(250000)  AS Tax_250000;
