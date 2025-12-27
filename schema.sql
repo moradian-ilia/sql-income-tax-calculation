e351308732c14d860ecb500cb8e3fcb752f54dcf
Abolfazl Moradian <moradian@gmail.com>
2025-12-27 19:28:40 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,9 @@
+CREATE TABLE TaxParameters (
+    Id INT PRIMARY KEY,
+    TaxExemptionAmount DECIMAL(18,2),
+
+    BracketFrom DECIMAL(18,2),
+    BracketTo   DECIMAL(18,2),
+    TaxRate     DECIMAL(5,2)
+);
 
