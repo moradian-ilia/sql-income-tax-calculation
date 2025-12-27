a6677ebf54eda873a66f21dcf6bd071f01553e3c
Abolfazl Moradian <moradian@gmail.com>
2025-12-27 19:33:51 +0200

    Update tax_function.sql

--- tax_function.sql
+++ tax_function.sql
@@ -1,1 +1,21 @@
+CREATE FUNCTION dbo.CalculateIncomeTax (
+    @TaxableIncome DECIMAL(18,2)
+)
+RETURNS DECIMAL(18,2)
+AS
+BEGIN
+    DECLARE @Tax DECIMAL(18,2) = 0;
+
+    SELECT @Tax = SUM(
+        CASE
+            WHEN @TaxableIncome > BracketFrom
+            THEN
+                (LEAST(@TaxableIncome, BracketTo) - BracketFrom) * TaxRate / 100
+            ELSE 0
+        END
+    )
+    FROM TaxParameters;
+
+    RETURN ROUND(@Tax, 2);
+END;
 
